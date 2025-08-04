023b56ce0ca5f9b8881e2035f426de7777a0ed48
upc <upc@europeia.dev>
2025-08-04 14:59:27 -0400

    feat: extract webhook/eurocore logic to utils package

--- cmd/v1nage/main.go
+++ cmd/v1nage/main.go
@@ -6,16 +6,14 @@ import (
 	"log"
 	"log/slog"
 	"regexp"
-	"strconv"
-	"strings"
 
 	"v1nage/pkg/config"
 	"v1nage/pkg/ns"
 	"v1nage/pkg/sse"
+	"v1nage/pkg/utils"
 	"v1nage/pkg/webhook"
 
 	"github.com/nsupc/eurogo/client"
-	"github.com/nsupc/eurogo/telegrams"
 	gsse "github.com/tmaxmax/go-sse"
 )
 
@@ -25,7 +23,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	waRegex := regexp.MustCompile(`^@@(.*)@@ was admitted to the World Assembly.?$`)
+	joinRegex := regexp.MustCompile(`^@@(.*)@@ was admitted to the World Assembly.?$`)
 	updateRegex := regexp.MustCompile(fmt.Sprintf(`^%%%%%s%%%% updated.?$`, conf.Region))
 	moveRegex := regexp.MustCompile(fmt.Sprintf(`^@@(.+)@@ relocated from %%%%.+%%%% to %%%%%s%%%%.?$`, conf.Region))
 
@@ -54,56 +52,16 @@ func main() {
 		}
 
 		if updateRegex.Match([]byte(event.Text)) {
-			go func() {
-				err = webhookClient.Send(fmt.Sprintf("[%s](https://www.nationstates.net/region=%s) updated!", conf.Region, conf.Region))
-				if err != nil {
-					slog.Error("unable to send webhook", slog.Any("error", err))
-				}
-			}()
-
+			go utils.HandleUpdate(webhookClient, conf.Region)
 			return
 		}
 
-		matches := waRegex.FindStringSubmatch(event.Text)
+		matches := joinRegex.FindStringSubmatch(event.Text)
 
 		if len(matches) > 0 {
 			nationName := matches[1]
 
-			go func() {
-				nationLink := fmt.Sprintf("[%s](https://www.nationstates.net/nation=%s#composebutton)", nationName, nationName)
-				msg := strings.ReplaceAll(conf.JoinMessage, "$nation", nationLink)
-
-				err = webhookClient.Send(msg)
-				if err != nil {
-					slog.Error("unable to send webhook", slog.Any("error", err))
-				}
-			}()
-
-			go func() {
-				var telegram telegrams.NewTelegram
-
-				if conf.JoinTelegram.Template != "" {
-					tmpl, err := eurocoreClient.GetTemplate(conf.JoinTelegram.Template)
-					if err != nil {
-						slog.Error("unable to retrieve template", slog.Any("error", err))
-						return
-					}
-
-					telegram = telegrams.New(tmpl.Nation, nationName, strconv.Itoa(tmpl.Tgid), tmpl.Key, telegrams.Standard)
-				} else if conf.JoinTelegram.Id != "" {
-					telegram = telegrams.New(conf.JoinTelegram.Author, nationName, conf.JoinTelegram.Id, conf.JoinTelegram.Secret, telegrams.Standard)
-				} else {
-					slog.Warn("join telegram not set, skipping")
-					return
-				}
-
-				err := eurocoreClient.SendTelegram(telegram)
-				if err != nil {
-					slog.Error("unable to send join telegram", slog.Any("error", err))
-				} else {
-					slog.Info("join telegram sent", slog.String("recipient", nationName))
-				}
-			}()
+			go utils.HandleWa(webhookClient, *eurocoreClient, conf.JoinMessage, nationName, conf.JoinTelegram.Template)
 
 			return
 		}
@@ -120,44 +78,8 @@ func main() {
 			}
 
 			if nation.WAStatus == "WA Member" {
-				go func() {
-					nationLink := fmt.Sprintf("[%s](https://www.nationstates.net/nation=%s#composebutton)", nationName, nationName)
-					msg := strings.ReplaceAll(conf.MoveMessage, "$nation", nationLink)
-
-					err = webhookClient.Send(msg)
-					if err != nil {
-						slog.Error("unable to send webhook", slog.Any("error", err))
-					}
-				}()
-
-				go func() {
-					var telegram telegrams.NewTelegram
-
-					if conf.MoveTelegram.Template != "" {
-						tmpl, err := eurocoreClient.GetTemplate(conf.MoveTelegram.Template)
-						if err != nil {
-							slog.Error("unable to retrieve template", slog.Any("error", err))
-							return
-						}
-
-						telegram = telegrams.New(tmpl.Nation, nationName, strconv.Itoa(tmpl.Tgid), tmpl.Key, telegrams.Standard)
-					} else if conf.MoveTelegram.Id != "" {
-						telegram = telegrams.New(conf.MoveTelegram.Author, nationName, conf.JoinTelegram.Id, conf.JoinTelegram.Secret, telegrams.Standard)
-					} else {
-						slog.Warn("move telegram not set, skipping")
-						return
-					}
-
-					err := eurocoreClient.SendTelegram(telegram)
-					if err != nil {
-						slog.Error("unable to send move telegram", slog.Any("error", err))
-					} else {
-						slog.Info("move telegram sent", slog.String("recipient", nationName))
-					}
-				}()
+				go utils.HandleWa(webhookClient, *eurocoreClient, conf.MoveMessage, nationName, conf.MoveTelegram.Template)
 			}
-
-			return
 		}
 	})
 
---
+++ pkg/utils/utils.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"fmt"
+	"log/slog"
+	"strconv"
+	"strings"
+	"v1nage/pkg/webhook"
+
+	"github.com/nsupc/eurogo/client"
+	"github.com/nsupc/eurogo/telegrams"
+)
+
+func HandleUpdate(client *webhook.WebhookClient, region string) {
+	msg := fmt.Sprintf("[%s](https://www.nationstates.net/region=%s) updated!", region, region)
+
+	err := client.Send(msg)
+	if err != nil {
+		slog.Error("failed to send region update notification", slog.Any("error", err))
+	}
+}
+
+func HandleWa(wClient *webhook.WebhookClient, eClient client.Client, msg string, nation string, tmplId string) {
+	link := fmt.Sprintf("[%s](https://www.nationstates.net/nation=%s#composebutton)", nation, nation)
+	msg = strings.ReplaceAll(msg, "$nation", link)
+
+	err := wClient.Send(msg)
+	if err != nil {
+		slog.Error("failed to send wa event notification", slog.Any("error", err))
+	}
+
+	if tmplId == "" {
+		slog.Warn("telegram template not set, skipping")
+		return
+	}
+
+	tmpl, err := eClient.GetTemplate(tmplId)
+	if err != nil {
+		slog.Error("failed to retrieve telegram template", slog.Any("error", err), slog.String("template", tmplId))
+	}
+
+	telegram := telegrams.New(tmpl.Nation, nation, strconv.Itoa(tmpl.Tgid), tmpl.Key, telegrams.Standard)
+	err = eClient.SendTelegram(telegram)
+	if err != nil {
+		slog.Error("failed to send telegram", slog.Any("error", err))
+	}
+}
